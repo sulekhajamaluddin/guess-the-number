2ef2d5bf9dbc7d73778fdb048ad79e077aea04d1
Sulekha Jamaluddin <sulu.lekha@gmail.com>
2021-12-29 13:40:15 +0100

    Refactored code

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 'use strict';
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let highScore = 0;
-//document.querySelector('.secret-number').textContent = secretNumber;
-const message = document.querySelector('.start-guessing');
+const displayMessage = function (message) {
+  document.querySelector('.start-guessing').textContent = message;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
 let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guessedNumber = Number(document.querySelector('.guessed-number').value);
@@ -10,9 +14,9 @@ document.querySelector('.check').addEventListener('click', function () {
   const isLowValue = guessedNumber - secretNumber < 0;
   if (score > 0) {
     if (!guessedNumber) {
-      message.textContent = 'PLEASE ENTER A NUMBER!!';
+      displayMessage('PLEASE ENTER A NUMBER!!');
     } else if (guessedNumber === secretNumber) {
-      message.textContent = 'CORRECT NUMBER!!';
+      displayMessage('CORRECT NUMBER!!');
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.secret-number').textContent = secretNumber;
       document.querySelector('.secret-number').style.width = '25rem';
@@ -21,23 +25,20 @@ document.querySelector('.check').addEventListener('click', function () {
       }
       document.querySelector('.high-score').textContent = highScore;
     } else if (checkNumber < 3) {
-      console.log('checkNumber < 3');
-      message.textContent = 'VERY CLOSE...';
+      displayMessage('VERY CLOSE...');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else if (checkNumber >= 3 && checkNumber < 8) {
-      console.log('checkNumber >= 3 && checkNumber < 8');
-      message.textContent = isLowValue ? 'LOW...' : 'HIGH...';
+      displayMessage(isLowValue ? 'LOW...' : 'HIGH...');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else if (checkNumber >= 8 && checkNumber < 20) {
-      console.log(checkNumber >= 8 && checkNumber < 20);
-      message.textContent = isLowValue ? 'TOO LOW...' : 'TOO HIGH';
+      displayMessage(isLowValue ? 'TOO LOW...' : 'TOO HIGH');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     }
     if (score == 0) {
-      message.textContent = 'YOU LOST!!';
+      displayMessage('YOU LOST!!');
     }
   }
 });
@@ -46,10 +47,9 @@ document.querySelector('.again-button').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.secret-number').textContent = '?';
   document.querySelector('.secret-number').style.width = '15rem';
-  //document.querySelector('.secret-number').textContent = secretNumber;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guessed-number').value = '';
   score = 20;
-  document.querySelector('.score').textContent = score;
-  message.textContent = 'Start Guessing... ';
+  displayScore(score);
+  displayMessage('Start Guessing... ');
 });
